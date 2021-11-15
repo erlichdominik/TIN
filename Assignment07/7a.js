afa5605d9ff5dcbee5253bc7c673d9f8f36098f0
erlichdominik <mimikdesu@gmail.com>
2021-11-15 12:33:28 +0100

    init Assignment07

---
+++ Assignment07/7a.js
@@ -0,0 +1,14 @@
+var http = require("http");
+var server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+
+  // url.pathname will return us /add or /multiply etc
+  const url = new URL(req.url, `http://${req.headers.host}`);
+  console.log("url object", url);
+
+  res.end("Hello World\n");
+});
+
+server.listen(8000, "127.0.0.1");
+
+console.log("Server's running");
