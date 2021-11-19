848b5972080657ba475001fe1b0212bf3f450344
erlichdominik <mimikdesu@gmail.com>
2021-11-20 00:36:52 +0100

    finish 7a

--- Assignment07/7a.js
+++ Assignment07/7a.js
@@ -5,18 +5,48 @@ var server = http.createServer((req, res) => {
   // url.pathname will return us /add or /multiply etc
   const url = new URL(req.url, `http://${req.headers.host}`);
   //console.log("url object", url);
-
-  const firstNumber = url.searchparams.get("firstnumber");
-  const secondNumber = url.searchparams.get("secondnumber");
+  let result = "unsuported operation";
   switch (url.pathname) {
     //adding
     case "/add":
-      const result = getResult(firstNumber, secondNumber, "+");
-      res.end(result);
+      result = getParams(url, "+");
       break;
-    case "multiply":
+    case "/multiply":
+      result = getParams(url, "*");
+      break;
+    case "/subtract":
+      result = getParams(url, "-");
+      break;
+    case "/divide":
+      result = getParams(url, "/");
+      break;
+    default:
+      break;
   }
+
+  res.end(result);
 });
+//TODO add validation
+
+const validateParams = (url, paramName) => {
+  let paramData = "";
+  try {
+    paramData = url.searchParams.get(paramName);
+  } catch (err) {
+    paramData = "error";
+  }
+  return paramData;
+};
+
+const getParams = (url, op) => {
+  const firstNumber = validateParams(url, "firstnumber");
+  const secondNumber = validateParams(url, "secondnumber");
+  if (isNaN(firstNumber) || isNaN(secondNumber)) {
+    return "wrong params";
+  }
+  const result = getResult(firstNumber, secondNumber, op);
+  return `firstParam: ${firstNumber}, secondParam: ${secondNumber}, result: ${result}`;
+};
 
 const getResult = (firstNumber, secondNumber, op) =>
   eval(`${firstNumber} ${op} ${secondNumber}`);
