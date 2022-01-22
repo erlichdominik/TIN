062d664c87070b3d5c34517991c379f39784484c
erlichdominik <mimikdesu@gmail.com>
2022-01-22 16:59:04 +0100

    task9 modified

--- Assignment09/task-a/routes/index.js
+++ Assignment09/task-a/routes/index.js
@@ -2,19 +2,27 @@ var express = require("express");
 var router = express.Router();
 // DATA ON THIS REQUEST SHOULD BE TWO NUMBERS AND ONE OP KEY
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
-});
+// router.get("/", function (req, res, next) {
+//   res.render("index", { title: "Express" });
+// });
 
-router.post("/", (req, res, next) => {
+router.get("/", (req, res, next) => {
   res.json({
-    FirstNumer: req.body.firstNumber,
-    SecondNumber: req.body.secondNumber,
-    Op: req.body.op,
-    Value: eval(
+    value: eval(
       `${req.body.firstNumber} ${req.body.op} ${req.body.secondNumber}`
     ),
   });
 });
+
+// router.post("/", (req, res, next) => {
+//   res.json({
+//     FirstNumer: req.body.firstNumber,
+//     SecondNumber: req.body.secondNumber,
+//     Op: req.body.op,
+//     Value: eval(
+//       `${req.body.firstNumber} ${req.body.op} ${req.body.secondNumber}`
+//     ),
+//   });
+// });
 
 module.exports = router;
--- Assignment09/task-b/index.js
+++ Assignment09/task-b/index.js
@@ -1,11 +1,21 @@
 // get form
 const form = document.getElementById("form");
 
-form.addEventListener("submit", (ev) => {});
+form.addEventListener("submit", (ev) => {
+  ev.preventDefault();
+  console.log(form);
+});
 
 const post = (data) => {
   const httpRequest = new XMLHttpRequest();
   httpRequest.open("POST", "https://www.localhost:3000/", true);
   httpRequest.setRequestHeader("Content-Type", "application/json");
-  httpRequest.send();
+
+  httpRequest.onload = () => {
+    var jsonResponse = JSON.parse(httpRequest.responseText);
+  };
+
+  const formData = new FormData(form);
+
+  httpRequest.send(JSON.stringify(formData));
 };
